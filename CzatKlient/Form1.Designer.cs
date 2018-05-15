7b786939eacdf5594787df5cbc68971397e39376
Madnarth <corwyn.blade@gmail.com>
2018-05-15 16:27:16 +0200

    v 1.0

--- CzatKlient/Form1.Designer.cs
+++ CzatKlient/Form1.Designer.cs
@@ -28,12 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.backgroundWorkerMainThread = new System.ComponentModel.BackgroundWorker();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.backgroundWorkerChat = new System.Windows.Forms.WebBrowser();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.textBoxNick = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // backgroundWorkerMainThread
+            // 
+            this.backgroundWorkerMainThread.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerMainThread_DoWork);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(817, 584);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Wyślij";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // backgroundWorkerChat
+            // 
+            this.backgroundWorkerChat.IsWebBrowserContextMenuEnabled = false;
+            this.backgroundWorkerChat.Location = new System.Drawing.Point(12, 214);
+            this.backgroundWorkerChat.MinimumSize = new System.Drawing.Size(20, 20);
+            this.backgroundWorkerChat.Name = "backgroundWorkerChat";
+            this.backgroundWorkerChat.Size = new System.Drawing.Size(880, 338);
+            this.backgroundWorkerChat.TabIndex = 1;
+            this.backgroundWorkerChat.Url = new System.Uri("about:blank", System.UriKind.Absolute);
+            this.backgroundWorkerChat.WebBrowserShortcutsEnabled = false;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(13, 558);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(879, 20);
+            this.textBoxMessage.TabIndex = 2;
+            this.textBoxMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxMessage_KeyDown);
+            // 
+            // textBoxNick
+            // 
+            this.textBoxNick.Location = new System.Drawing.Point(48, 19);
+            this.textBoxNick.Name = "textBoxNick";
+            this.textBoxNick.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNick.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nick:";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(48, 45);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 5;
+            this.buttonConnect.Text = "Połącz";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(48, 75);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 6;
+            this.buttonDisconnect.Text = "Wyjdź";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 619);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNick);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.backgroundWorkerChat);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorkerMainThread;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.WebBrowser backgroundWorkerChat;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.TextBox textBoxNick;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
     }
 }
 
--- CzatKlient/Form1.cs
+++ CzatKlient/Form1.cs
@@ -7,14 +7,157 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace CzatKlient
 {
     public partial class Form1 : Form
     {
+        private TcpClient client;
+        private string addressIPServer = "127.0.0.1";
+        private BinaryWriter write;
+        private bool isActive = false;
         public Form1()
         {
             InitializeComponent();
+            backgroundWorkerChat.Document.Write("<html><head><style>body,table { font-size: 10pt; font - family: Verdana; margin: 3px 3px 3px 3px; font - color: black; }</ style ></ head >< body width =\"" + (backgroundWorkerChat.ClientSize.Width - 20).ToString() + "\">");
+        }
+        #region Metoda odwołująca się do własności kontrolki webBrowser1 z poziomu innego wątku
+        delegate void SetTextHTMLCallBack(string tekst);
+        private void SetTextHTML(string tekst)
+        {
+            if (backgroundWorkerChat.InvokeRequired)
+            {
+                SetTextHTMLCallBack f = new SetTextHTMLCallBack(SetTextHTML);
+                this.Invoke(f, new object[] { tekst });
+            }
+            else
+            {
+                this.backgroundWorkerChat.Document.Write(tekst);
+            }
+        }
+        delegate void SetScrollCallBack();
+        private void SetScroll()
+        {
+            if (backgroundWorkerChat.InvokeRequired)
+            {
+                SetScrollCallBack f = new SetScrollCallBack(SetScroll);
+                this.Invoke(f);
+            }
+            else
+            {
+                this.backgroundWorkerChat.Document.Window.ScrollTo(1, int.MaxValue);
+            }
+        }
+
+        #endregion
+        private void AddText(string who, string message)
+        {
+            SetTextHTML("<table><tr><td width=\"10%\"><b>[" + who + "]: </ b ></ td > ");           
+            SetTextHTML("<td colspan=2>" + message + "</td></tr></table>");
+            SetScroll();
+        }
+
+        private void backgroundWorkerMainThread_DoWork(object sender, DoWorkEventArgs e)
+        {
+            UdpClient client = new UdpClient(2500);
+            IPEndPoint addressIP = new IPEndPoint(IPAddress.Parse(addressIPServer), 0);
+            string message = "";
+            while (!backgroundWorkerMainThread.CancellationPending)
+            {
+                Byte[] bufor = client.Receive(ref addressIP);
+                string data = Encoding.UTF8.GetString(bufor);
+                string[] cmd = data.Split(new char[] { ':' });
+                if (cmd[1] == "BYE")
+                {
+                    AddText("system", "klient odłączony");
+                    client.Close();
+                    return;
+                }
+                if (cmd.Length > 2)
+                {
+                    message = cmd[2];
+                    for (int i = 3; i < cmd.Length; i++)
+                        message += ":" + cmd[i];
+                }
+                AddText(cmd[0], message);
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxNick.Text != String.Empty)
+                {
+                    client = new TcpClient(addressIPServer, 6969);
+                    textBoxNick.ReadOnly = true;
+                    NetworkStream ns = client.GetStream();
+                    write = new BinaryWriter(ns);
+                    write.Write(textBoxNick.Text + ":HI:" + "pusty");
+                    BinaryReader read = new BinaryReader(ns);
+                    string answer = read.ReadString();
+                    if (answer == "HI")
+                    {
+                        backgroundWorkerMainThread.RunWorkerAsync();
+                        isActive = true;
+                        buttonConnect.Enabled = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Serwer odmawia nawiązania połączenia");
+                        buttonConnect.Enabled = true; client.Close();
+                    }
+                }
+                else
+                    MessageBox.Show("Wpisz swój nick");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nie można nawiązać połączenia " + ex.Message);
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (isActive && textBoxMessage.Text != String.Empty)
+                write.Write(textBoxNick.Text + ":SAY:" + textBoxMessage.Text);
+            textBoxMessage.Text = String.Empty;
+        }
+
+        private void textBoxMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.buttonSend_Click(sender, null);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (write != null)
+            {
+                try
+                {
+                    write.Write(textBoxNick.Text + ":BYE:" + "pusty");
+                    write.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Błąd");
+                }
+            }
+            if (backgroundWorkerMainThread.IsBusy)
+                backgroundWorkerMainThread.CancelAsync();
+            if (client != null)
+                client.Close();
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
